6cf39df92bd824f40bb15eab717a4d1b3bfa2014
Joe <joehewett1@gmail.com>
2024-02-24 20:10:53 +0000

    Hook up to API

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({
       <body
         className={`flex h-screen flex-col bg-background font-mono text-foreground`}
       >
+        <h1 className="text-4xl font-bold text-center p-4 mt-6 bg-primary text-dark">Refill</h1>
         <div className="flex-grow flex-shrink overflow-hidden p-4 md:px-16 md:py-6 lg:px-32">
             {children}
         </div>
--- src/components/RefillForm.tsx
+++ src/components/RefillForm.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Example() {
+
+    let [response, setResponse] = useState(0)
+
   return (
-    <h1 className="text-2xl font-bold text-center">Refill</h1>
+    <>
+    <div className="flex flex-col items-center justify-center">
+    <button
+      className="bg-primary text-dark p-2 rounded-md"
+      onClick={() => {
+        fetch('https://gin-production-0e58.up.railway.app', {
+          method: 'GET',
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setResponse(data)
+          })
+      }}
+    >
+        Click here
+    </button>
+    <p>{response}</p>
+    </div>
+    </>
   )
 }
