bf84d77fa99591dc49f931ee500574c6564c421e
Joe <joehewett1@gmail.com>
2024-05-18 16:21:09 +0200

    Add main page image

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import React from "react";
 function App() {
   return (
     <div
-      className={`flex flex-col bg-background font-mono text-foreground my-24`}
+      className={`flex flex-col bg-background font-mono text-foreground my-16`}
     >
       <h1 className="text-4xl font-bold text-center p-4 mt-6 bg-primary text-dark">
-        Refill
+        Coro Benavent
       </h1>
       <div className="flex-grow flex-shrink overflow-hidden p-4 md:px-16 md:py-6 lg:px-32">
+        <img src="/hh.jpeg" alt="Coro Benavent" className="" />
       </div>
     </div>
   );
