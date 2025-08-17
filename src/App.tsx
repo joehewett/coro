502a3a337656b71d51ff45b416d66b54ae08636f
Joe Hewett <joehewett1@gmail.com>
2025-08-17 23:02:02 +0100

    feat: music player and initial songs

--- src/App.tsx
+++ src/App.tsx
@@ -16,41 +16,46 @@ import LiveAtSunset from './components/projects/LiveAtSunset';
 import PersonalJourney from './components/projects/PersonalJourney';
 import BehindTheScenes from './components/projects/BehindTheScenes';
 import PixelAdventure from './components/game/PixelAdventure';
+import MusicPlayer from './components/MusicPlayer';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      {/* Homepage route without navigation */}
-      <Route path="/" element={<Homepage />} />
-      
-      {/* Category pages */}
-      <Route path="/actress" element={<ActressPage />} />
-      <Route path="/director" element={<DirectorPage />} />
-      <Route path="/musician" element={<MusicianPage />} />
-      <Route path="/me" element={<MePage />} />
-      
-      {/* Actress project pages */}
-      <Route path="/actress/palomitas-sin-maiz" element={<PalomitasSinMaiz />} />
-      <Route path="/actress/summer-dreams" element={<SummerDreams />} />
-      <Route path="/actress/urban-tales" element={<UrbanTales />} />
-      <Route path="/actress/the-last-dance" element={<TheLastDance />} />
-      
-      {/* Director project pages */}
-      <Route path="/director/superglue" element={<Superglue />} />
-      <Route path="/director/palomitas-sin-maiz" element={<PalomitasSinMaiz />} />
-      <Route path="/director/padre-hijo-pastor" element={<PadreHijoPastor />} />
-      
-      {/* Musician project pages */}
-      <Route path="/musician/echoes-of-tomorrow" element={<EchoesOfTomorrow />} />
-      <Route path="/musician/live-at-sunset" element={<LiveAtSunset />} />
-      
-      {/* Personal project pages */}
-      <Route path="/me/personal-journey" element={<PersonalJourney />} />
-      <Route path="/me/behind-the-scenes" element={<BehindTheScenes />} />
-      
-      {/* Secret game route */}
-      <Route path="/secret" element={<PixelAdventure />} />
-    </Routes>
+    <>
+      <Routes>
+        {/* Homepage route without navigation */}
+        <Route path="/" element={<Homepage />} />
+        
+        {/* Category pages */}
+        <Route path="/actress" element={<ActressPage />} />
+        <Route path="/director" element={<DirectorPage />} />
+        <Route path="/musician" element={<MusicianPage />} />
+        <Route path="/me" element={<MePage />} />
+        
+        {/* Actress project pages */}
+        <Route path="/actress/palomitas-sin-maiz" element={<PalomitasSinMaiz />} />
+        <Route path="/actress/summer-dreams" element={<SummerDreams />} />
+        <Route path="/actress/urban-tales" element={<UrbanTales />} />
+        <Route path="/actress/the-last-dance" element={<TheLastDance />} />
+        
+        {/* Director project pages */}
+        <Route path="/director/superglue" element={<Superglue />} />
+        <Route path="/director/palomitas-sin-maiz" element={<PalomitasSinMaiz />} />
+        <Route path="/director/padre-hijo-pastor" element={<PadreHijoPastor />} />
+        
+        {/* Musician project pages */}
+        <Route path="/musician/echoes-of-tomorrow" element={<EchoesOfTomorrow />} />
+        <Route path="/musician/live-at-sunset" element={<LiveAtSunset />} />
+        
+        {/* Personal project pages */}
+        <Route path="/me/personal-journey" element={<PersonalJourney />} />
+        <Route path="/me/behind-the-scenes" element={<BehindTheScenes />} />
+        
+        {/* Secret game route */}
+        <Route path="/secret" element={<PixelAdventure />} />
+      </Routes>
+      {/* Global music player */}
+      <MusicPlayer />
+    </>
   );
 };
 
